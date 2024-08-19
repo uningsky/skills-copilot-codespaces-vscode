e398aaae14fe6eda4afc1204515c735851fe9bb1
uningsky <uningsky@gmail.com>
2024-08-19 14:09:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  var member = this;
+  member.skills = [
+    {
+      name: "HTML",
+      level: "Expert"
+    },
+    {
+      name: "CSS",
+      level: "Expert"
+    },
+    {
+      name: "JavaScript",
+      level: "Expert"
+    }
+  ];
+}
